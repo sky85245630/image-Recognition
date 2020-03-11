103910811c770afac177eee12b6d67f079665586
cw.chen <sky85245630@gmail.com>
2020-03-11 19:24:46 +0800

    life style FXXKING Bug

--- js/login copy.js
+++ js/login copy.js
@@ -66,10 +66,11 @@ function lifestyle1(){
       success:function(data){
         console.log("取得生活風格列表成功");
         for ( i=0 ; i < data.lifestyle_list.length ; i++){
-            LS += '<option name="' + data.lifestyle_list[i] + '" value="' + data.lifestyle_list[i] + '">' + data.lifestyle_list[i] + '</option>'
+            QQ += '<option name="' + data.lifestyle_list[i] + '" value="' + data.lifestyle_list[i] + '">' + data.lifestyle_list[i] + '</option>'
         }
       }
     })
+  return QQ
 }
 
   //人物風格列表
@@ -104,21 +105,21 @@ var local_character_start_date = localStorage.getItem('character_start_date')
 var local_character_end_time = localStorage.getItem('character_end_time')
 var local_modify_status = localStorage.getItem('modify_status') =="true"?true:false
 
-var ZZ = JSON.stringify({
+var QQ = JSON.stringify({
   start_date: local_character_start_date,
   end_date: local_character_end_time,
   modify_status: local_modify_status,
   page_num: 1
 });
 
-var LS = lifestyle1()
+var a = lifestyle1()
 $.ajax({
   url: "http://api.i-buzz.com.tw/img-recognition/style_recongition",
   type: "POST",
   headers: {
     Authorization: "Bearer " + jwt
   },
-  data: ZZ,
+  data: QQ,
   contentType: "application/json; charset=utf-8",
   error: function(xhr) {
     console.log("傳送人物校正時間區間失敗");
@@ -155,7 +156,7 @@ $.ajax({
     console.log(data);
     console.log(data.data[0].kol_id);
       for ( i=0 ; i < data.data.length ; i++){
-          character = '<tr> <td>' + data.data[i].kol_id + '</td><td> <a href="'+ data.data[i].pic_url +'" data-toggle="lightbox"> <img src="'+ data.data[i].pic_url +'" class="img-fluid rounded" style="max-width: 200px;"/> </a> </td><td class="recognition_result"> 人物風格：'+ data.data[i].character +'<br/> 生活風格：'+ data.data[i].lifestyle +'<br/> 性別：'+ data.data[i].gender +'<br/> </td><td> <select id="character_style" class="btn btn-mini result_btn"> <option selected disabled>'+ data.data[i].character +'</option> </select ><br/> <select id="life_style" class="btn btn-mini result_btn"> <option selected disabled>'+ data.data[i].lifestyle +'</option>'+ LS +' </select ><br/> <select id="character_gender" class="btn btn-mini result_btn"> <option selected disabled>'+ data.data[i].gender +'</option ><option name="男" value="男">男</option ><option name="女" value="女">女</option> </select ><br/> </td><td> <button type="button" class="btn btn-success">儲存</button> </td></tr>'
+          character = '<tr> <td>' + data.data[i].kol_id + '</td><td> <a href="'+ data.data[i].pic_url +'" data-toggle="lightbox"> <img src="'+ data.data[i].pic_url +'" class="img-fluid rounded" style="max-width: 200px;"/> </a> </td><td class="recognition_result"> 人物風格：'+ data.data[i].character +'<br/> 生活風格：'+ data.data[i].lifestyle +'<br/> 性別：'+ data.data[i].gender +'<br/> </td><td> <select id="character_style" class="btn btn-mini result_btn"> <option selected disabled>'+ data.data[i].character +'</option> </select ><br/> <select id="life_style" class="btn btn-mini result_btn"> <option selected disabled>'+ data.data[i].lifestyle +'</option>'+ lifestyle1()+' </select ><br/> <select id="character_gender" class="btn btn-mini result_btn"> <option selected disabled>'+ data.data[i].gender +'</option ><option name="男" value="男">男</option ><option name="女" value="女">女</option> </select ><br/> </td><td> <button type="button" class="btn btn-success">儲存</button> </td></tr>'
           $('#character').append(character);
         //   console.log(data.character_style[i])
         //   console.log( character )
